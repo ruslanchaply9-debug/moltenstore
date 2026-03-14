61129684b2ca9040e7580034cb066a99b38db5e5
ruslanchaply9-debug <ruslanchaply9@gmail.com>
2026-03-14 12:59:44 +0200

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { email } = req.body;
+
+    res.status(200).json({
+      token: "demo-token",
+      user: {
+        email: email,
+        role: "user"
+      }
+    });
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
